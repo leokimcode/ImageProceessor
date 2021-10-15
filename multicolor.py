f64bf31b4dbb1f7143cba86a25f5fcdf151bfb5d
leokimcode <leokimcoding@gmail.com>
2021-10-14 23:03:55 -0400

    Update

--- multicolor.py
+++ multicolor.py
@@ -9,7 +9,10 @@ array = np.zeros([height, width, 3], dtype=np.uint8)
 
 for x in range(width):
     for y in range(height):
-        array[x,y] = [random.randint(255), random.randint(255), random.randint(255)]
+        r = random.randint(255) # currently random from 0-255, should be r value from camera
+        g = random.randint(255) # g value from camera
+        b = random.randint(255) # b value from camera
+        array[x,y] = [r, g, b]
 
 print(array[:width])
 plt.imshow(array)
